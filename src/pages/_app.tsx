449d71fd622084300c5d9c6860c6f5d293a7a495
Aman Dogra <amandogra@Meenakshis-MacBook-Pro.local>
2021-02-27 17:33:23 -0800

    renamed global.css to globals.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/global.css'
+import '../styles/globals.css'
 // import 'tailwindcss/tailwind.css'
 import 'katex/dist/katex.css'
 import Footer from '../components/footer'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -62,6 +62,16 @@ export default () => (
             )
           })}
         </div>
+        <a
+          href="https://www.patreon.com/bePatron?u=19522637"
+          data-patreon-widget-type="become-patron-button"
+        >
+          Become a Patron!
+        </a>
+        <script
+          async
+          src="https://c6.patreon.com/becomePatronButton.bundle.js"
+        ></script>
       </div>
     </div>
   </>
